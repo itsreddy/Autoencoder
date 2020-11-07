c3db839ae21c9202c6efdd086f8656a7546c6eb8
prash <dprashureddy007@gmail.com>
2020-11-07 11:24:16 -0800

    changed TensorboardLogger.save_values to take dict as argument

--- src/models/tensorboard.py
+++ src/models/tensorboard.py
@@ -28,24 +28,16 @@ class TensorboardLogger:
             rmtree(self.logs_path)
         return SummaryWriter(log_dir=self.logs_path)
 
-    def save_values_to_tensorboard(self, epoch_no, reconstr_loss_mean, reg_dist_loss_mean, disc_loss_mean,
-                                   norms_mean):
+    def save_values(self, epoch_no, values: dict):
         """
         All params except for epoch_no might be changed depending on the training
         :param epoch_no:
-        :param reconstr_loss_mean:
-        :param reg_dist_loss_mean:
-        :param disc_loss_mean:
-        :param norms_mean:
+        :param values: dict of str -> values or dicts
         :return:
         """
-        self.writer.add_scalars("Reconstruction_loss", {"conv": reconstr_loss_mean[0],
-                                                        "linear": reconstr_loss_mean[1]}, epoch_no)
-        self.writer.add_scalar("Regularization_loss/reg_loss", reg_dist_loss_mean[0], epoch_no)
-        self.writer.add_scalar("Distance_loss/dist_loss", reg_dist_loss_mean[1], epoch_no)
-        self.writer.add_scalars("Discriminator_loss", {"d_prior_distr": disc_loss_mean[0],
-                                                       "d_linear": disc_loss_mean[1],
-                                                       "d_conv": disc_loss_mean[2]}, epoch_no)
-        self.writer.add_scalars("norms", {"prior_distr": norms_mean[0],
-                                          "linear": norms_mean[1],
-                                          "conv": norms_mean[2]}, epoch_no)
+
+        for tag, value in values.items():
+            if type(value) == 'dict':
+                self.writer.add_scalars(tag, value, epoch_no)
+            else:
+                self.writer.add_scalar(tag, value, epoch_no)
